f8fce8df9f411d7099f61024b09cdf1f035e4f11
seb.cato@gmail.com <seb.cato@gmail.com>
2017-03-11 07:05:27 +0000

    netstring: added netstring_append_buf

--- lib/util/netstring.c
+++ lib/util/netstring.c
@@ -1,15 +1,19 @@
+#include <stdio.h>
+
 #include <lib/util/netstring.h>
 
 const char *netstring_strerror(int code) {
   switch(code) {
-  case NS_OK:
+  case NETSTRING_OK:
     return "success";
-  case NS_ERRFMT:
+  case NETSTRING_ERRFMT:
     return "netstring format error";
-  case NS_ERRTOOLARGE:
+  case NETSTRING_ERRTOOLARGE:
     return "netstring length too large";
-  case NS_ERRINCOMPLETE:
+  case NETSTRING_ERRINCOMPLETE:
     return "incomplete netstring";
+  case NETSTRING_ERRMEM:
+    return "netstring memory allocation error";
   default:
     return "unknown netstring error";
   }
@@ -21,40 +25,73 @@ int netstring_parse(char **out, size_t *outlen, char *src, size_t srclen) {
   size_t lenbuf;
 
   enum {
-    NS_SLEN,
-    NS_SDATA,
-  } S = NS_SLEN;
+    NETSTRING_SLEN,
+    NETSTRING_SDATA,
+  } S = NETSTRING_SLEN;
 
   for (i=0; i < srclen; i++) {
     switch (S) {
-    case NS_SLEN:
+    case NETSTRING_SLEN:
       if (src[i] >= '0' && src[i] <= '9') {
         lenbuf = len * 10;
         lenbuf += src[i]-'0';
         if (lenbuf < len) {
           /* lenbuf overflow */
-          return NS_ERRTOOLARGE;
+          return NETSTRING_ERRTOOLARGE;
         }
         len = lenbuf;
         break;
       } else if (i > 0 && src[i] == ':') {
-        S = NS_SDATA;
+        S = NETSTRING_SDATA;
         break;
       } else {
-        return NS_ERRFMT;
+        return NETSTRING_ERRFMT;
       }
-    case NS_SDATA:
+    case NETSTRING_SDATA:
       if (i + len >= srclen) {
-        return NS_ERRINCOMPLETE;
+        return NETSTRING_ERRINCOMPLETE;
       }
       if (src[i+len] != ',') {
-        return NS_ERRFMT;
+        return NETSTRING_ERRFMT;
       }
       src[i+len] = '\0';
       *out = src + i;
       *outlen = len;
-      return NS_OK;
+      return NETSTRING_OK;
     }
   }
-  return NS_ERRINCOMPLETE;
+  return NETSTRING_ERRINCOMPLETE;
 }
+
+int netstring_append_buf(buf_t *buf, const char *str, size_t len) {
+  char szbuf[32];
+  int ret;
+  size_t oldlen;
+  int failcode = NETSTRING_ERRMEM;
+
+  oldlen = buf->len;
+
+  if ((ret = snprintf(szbuf, sizeof(szbuf), "%zu:", len)) <= 1) {
+    failcode = NETSTRING_ERRFMT;
+    goto fail;
+  }
+
+  if (buf_adata(buf, szbuf, (size_t)ret) < 0) {
+    goto fail;
+  }
+
+  if (buf_adata(buf, str, len) < 0) {
+    goto fail;
+  }
+
+  if (buf_achar(buf, ',') < 0) {
+    goto fail;
+  }
+
+  return NETSTRING_OK;
+
+fail:
+  buf->len = oldlen;
+  return ret;
+}
+
--- lib/util/netstring.h
+++ lib/util/netstring.h
@@ -3,12 +3,16 @@
 
 #include <stddef.h>
 
-#define NS_OK             0
-#define NS_ERRFMT        -1
-#define NS_ERRTOOLARGE   -2
-#define NS_ERRINCOMPLETE -3
+#include <lib/util/buf.h>
+
+#define NETSTRING_OK             0
+#define NETSTRING_ERRFMT        -1
+#define NETSTRING_ERRTOOLARGE   -2
+#define NETSTRING_ERRINCOMPLETE -3
+#define NETSTRING_ERRMEM        -4
 
 const char *netstring_strerror(int code);
 int netstring_parse(char **out, size_t *outlen, char *src, size_t srclen);
+int netstring_append_buf(buf_t *buf, const char *str, size_t len);
 
 #endif /* NETSTRING_H_ */

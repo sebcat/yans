c800e0f98a94109fa0fbdb169948cb039226c7e3
seb.cato@gmail.com <seb.cato@gmail.com>
2017-08-15 19:24:57 +0000

    flagset: Initial commit

---
+++ lib/util/flagset.c
@@ -0,0 +1,49 @@
+#include <stddef.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <lib/util/flagset.h>
+
+int flagset_from_str(struct flagset_map *m, const char *s,
+    struct flagset_result *out) {
+  size_t len;
+  size_t i;
+  unsigned int res = 0;
+  const char *start = s;
+
+  memset(out, 0, sizeof(*out));
+  if (s == NULL || *s == '\0') {
+    return 0;
+  }
+
+  for(s += strspn(s, FLAGSET_SEPS); *s != '\0'; s += strspn(s, FLAGSET_SEPS)) {
+    /* determine the length of the current flag, error out if it's too long */
+    len = strcspn(s, FLAGSET_SEPS);
+    if (len > FLAGSET_MAXSZ) {
+      out->errmsg = "flag too long";
+      out->erroff = s - start;
+      return -1;
+    }
+
+    /* N is small, do a linear search for the flag */
+    for (i = 0; m[i].name != NULL; i++) {
+      if (len == m[i].namelen && memcmp(m[i].name, s, len) == 0) {
+        res |= m[i].flag;
+        break;
+      }
+    }
+
+    /* report an error if the flag is not in the set */
+    if (m[i].name == NULL) {
+      out->errmsg = "flag does not exist";
+      out->erroff = s - start;
+      return -1;
+    }
+
+    s += len;
+  }
+
+  out->flags = res;
+}
+
---
+++ lib/util/flagset.h
@@ -0,0 +1,32 @@
+#ifndef UTIL_FLAGSET_H__
+#define UTIL_FLAGSET_H__
+
+/* maximum length of a flag string */
+#define FLAGSET_MAXSZ 31
+
+/* valid flag separators */
+#define FLAGSET_SEPS "\r\n\t |,"
+
+struct flagset_map {
+  const char *name;
+  size_t namelen;
+  unsigned int flag;
+};
+
+#define FLAGSET_ENTRY(name__, flag__) \
+    {.name = (name__), .namelen = sizeof((name__)) - 1, .flag = (flag__)}
+
+#define FLAGSET_END {0}
+
+struct flagset_result {
+  unsigned int flags;
+  const char *errmsg;
+  size_t erroff;
+};
+
+/* returns -1 on error, 0 on success. Fills in flagset_result accordingly */
+int flagset_from_str(struct flagset_map *m, const char *s,
+    struct flagset_result *out);
+
+
+#endif /* UTIL_FLAGSET_H__ */

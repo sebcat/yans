b594fb17b895ad72c85ac4fc61da34b8c7c5c4e5
seb.cato@gmail.com <seb.cato@gmail.com>
2017-12-17 15:05:55 +0000

    build: made yans.mk the default Makefile

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-export CFLAGS="-Os -pipe -Wall -Werror -fvisibility=hidden \
+export CC=clang40
+export CFLAGS="-Os -g -pipe -Wall -Werror -fvisibility=hidden \
      -ffunction-sections -fdata-sections"
 export LDFLAGS=" -Wl,--gc-sections"
 # -Wl,--build-id=none
-make -f yans.mk
+make

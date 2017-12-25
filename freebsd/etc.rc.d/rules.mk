cc857e94f2ed83a7c99de55dc2adc76ec2e98faa
seb.cato@gmail.com <seb.cato@gmail.com>
2017-12-25 06:42:52 +0000

    freebsd/stored: Initial rc script

--- freebsd/etc.rc.d/rules.mk
+++ freebsd/etc.rc.d/rules.mk
@@ -3,3 +3,8 @@ freebsd/etc.rc.d/ethd: freebsd/etc.rc.d/ethd.in
 		-e "s,@LOCALSTATEDIR@,$(LOCALSTATEDIR)," \
 		< freebsd/etc.rc.d/ethd.in > freebsd/etc.rc.d/ethd
 
+freebsd/etc.rc.d/stored: freebsd/etc.rc.d/stored.in
+	sed -e "s,@BINDIR@,$(BINDIR)," \
+		-e "s,@LOCALSTATEDIR@,$(LOCALSTATEDIR)," \
+		< freebsd/etc.rc.d/stored.in > freebsd/etc.rc.d/stored
+

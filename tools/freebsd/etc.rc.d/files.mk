5b70bc145c56d600f99748e1ea1ca57f359933af
seb.cato@gmail.com <seb.cato@gmail.com>
2018-01-27 19:32:21 +0000

    freebsd: added knegd rc script

--- tools/freebsd/etc.rc.d/files.mk
+++ tools/freebsd/etc.rc.d/files.mk
@@ -2,7 +2,8 @@
 GENERATED_RCFILESIN_FreeBSD = \
     tools/freebsd/etc.rc.d/ethd.in \
     tools/freebsd/etc.rc.d/stored.in \
-    tools/freebsd/etc.rc.d/clid.in
+    tools/freebsd/etc.rc.d/clid.in \
+    tools/freebsd/etc.rc.d/knegd.in
 
 GENERATED_RCFILES_FreeBSD = ${GENERATED_RCFILESIN_FreeBSD:.in=}
 

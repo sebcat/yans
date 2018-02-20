fe151d84e91788159890547d1baff9bf3646bceb
seb.cato@gmail.com <seb.cato@gmail.com>
2018-02-20 20:01:30 +0000

    tmux-daemons: Added some error checking

--- tools/tmux-daemons.sh
+++ tools/tmux-daemons.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
-mkdir lel
+
+die() {
+  printf '%s\n' "$1" >&2
+  exit 1
+}
+
+if [ ! -x ./apps/ethd/ethd -o \
+     ! -x ./apps/clid/clid -o \
+     ! -x ./apps/stored/stored ]; then
+  die "one or more daemons missing"
+fi
+
+if [ ! -d ./lel ]; then
+  mkdir ./lel || die "failed to create ./lel"
+fi
+
 tmux new-session -s "yansd" -d ./apps/ethd/ethd -n -b "$(pwd)/lel"
 tmux split-window -v -p 66 ./apps/clid/clid -n -b "$(pwd)/lel"
 tmux split-window -v -p 50 ./apps/stored/stored -n -b "$(pwd)/lel"

73f25a38dd5f3dcfd1f134acf141877b8b35b2f6
seb.cato@gmail.com <seb.cato@gmail.com>
2018-02-24 04:21:20 +0000

    tmux-daemons: Add opional valgrind use

--- tools/tmux-daemons.sh
+++ tools/tmux-daemons.sh
@@ -15,8 +15,17 @@ if [ ! -d ./lel ]; then
   mkdir ./lel || die "failed to create ./lel"
 fi
 
-tmux new-session -s "yansd" -d ./apps/ethd/ethd -n -b "$(pwd)/lel"
+MAYBE_VALGRIND=
+if [ "$USE_VALGRIND" = 1 ]; then
+  MAYBE_VALGRIND="valgrind --error-exitcode=1 --leak-check=full \
+--log-file=run.%p --trace-children=yes"
+fi
+
+tmux new-session -s "yansd" -d $MAYBE_VALGRIND \
+    ./apps/ethd/ethd -n -b "$(pwd)/lel"
+# clid uses syscalls not implemented in some versions of valgrind
 tmux split-window -v -p 66 ./apps/clid/clid -n -b "$(pwd)/lel"
-tmux split-window -v -p 50 ./apps/stored/stored -n -b "$(pwd)/lel"
+tmux split-window -v -p 50 $MAYBE_VALGRIND \
+    ./apps/stored/stored -n -b "$(pwd)/lel"
 tmux -2 attach-session -d
 

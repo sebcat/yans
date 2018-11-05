5e746641bc82ee29bc4694eed635c01f4a857662
Sebastian Cato <seb.cato@gmail.com>
2018-11-05 08:05:16 +0100

    version.sh: Add shorthash to version

--- version.sh
+++ version.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-echo v$(date +%Y%m%d)+snapshot
+
+VERSION=v$(date +%Y%m%d)
+SUFFIX=+snapshot
+
+if git rev-parse --is-inside-work-tree >/dev/null 2>&1 ; then
+  SUFFIX=+$(git rev-parse --short HEAD)
+fi
+
+echo "${VERSION}${SUFFIX}"

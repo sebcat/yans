dfb6ee31b16b401c417a426e2655bacaeb3cc1ce
Sebastian Cato <seb.cato@gmail.com>
2019-02-07 07:10:45 +0100

    scan: Use path in error messages

--- apps/scan/banners.c
+++ apps/scan/banners.c
@@ -257,14 +257,14 @@ int banners_main(struct scan_ctx *scan, int argc, char *argv[]) {
 
   ret = opener_fopen(&scan->opener, opts.outpath, "wb", &outfile);
   if (ret < 0) {
-    fprintf(stderr, "opener_fopen output: %s\n",
+    fprintf(stderr, "%s: %s\n", opts.outpath,
         opener_strerr(&scan->opener));
     goto done_ssl_ctx_free;
   }
 
   ret = opener_fopen(&scan->opener, opts.inpath, "rb", &infile);
   if (ret < 0) {
-    fprintf(stderr, "opener_fopen input: %s\n",
+    fprintf(stderr, "%s: %s\n", opts.inpath,
         opener_strerr(&scan->opener));
     goto done_fclose_outfile;
   }
--- apps/scan/resolve.c
+++ apps/scan/resolve.c
@@ -24,8 +24,7 @@ static int read_names(struct opener_ctx *opener, buf_t *buf,
 
   ret = opener_fopen(opener, infile, "rb", &fp);
   if (ret < 0) {
-    fprintf(stderr, "opener_fopen input: %s\n",
-        opener_strerr(opener));
+    fprintf(stderr, "%s: %s\n", infile, opener_strerr(opener));
     return -1;
   }
 
@@ -135,8 +134,7 @@ int resolve_main(struct scan_ctx *scan, int argc, char **argv) {
   ret = opener_open(&scan->opener, outfile, O_WRONLY | O_CREAT,
     &use_zlib, &outfd);
   if (ret < 0) {
-    fprintf(stderr, "opener_fopen input: %s\n",
-        opener_strerr(&scan->opener));
+    fprintf(stderr, "%s: %s\n", outfile, opener_strerr(&scan->opener));
     goto end_buf_cleanup;
   }
 

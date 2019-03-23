1a4d3314035b4fc0d2773ab5a9983fb361cbe1b9
Sebastian Cato <seb.cato@gmail.com>
2019-03-23 07:45:59 +0000

    sc2: Fix Linux build

--- apps/sc2/files.mk
+++ apps/sc2/files.mk
@@ -1,20 +1,14 @@
 sc2_DEPS = \
-    lib/util/io.c \
-    lib/util/os.c \
-    lib/util/buf.c \
-    lib/util/eds.c \
-    lib/util/ylog.c \
-    lib/util/sandbox.c
-
+    lib/util/io.c lib/util/os.c lib/util/buf.c lib/util/eds.c \
+    lib/util/ylog.c lib/util/sandbox.c
 sc2_DEPSOBJS = ${sc2_DEPS:.c=.o}
-
-sc2_SOURCES = \
-    apps/sc2/main.c
-
+sc2_SOURCES = apps/sc2/main.c
 sc2_HEADERS =
 
-sc2_OBJS = ${sc2_SOURCES:.c=.o}
+sc2_LDADD_Linux = -lseccomp
+sc2_LDADD      += -ldl ${sc2_LDADD_${UNAME_S}}
 
+sc2_OBJS = ${sc2_SOURCES:.c=.o}
 sc2_BIN = apps/sc2/sc2
 
 OBJS += $(sc2_OBJS)
--- apps/sc2/rules.mk
+++ apps/sc2/rules.mk
@@ -2,5 +2,5 @@ $(sc2_OBJS): $(sc2_DEPSOBJS) $(sc2_SOURCES) $(sc2_HEADERS)
 
 $(sc2_BIN): $(sc2_OBJS)
 	$(CC) $(CFLAGS) -o $(sc2_BIN) ${sc2_DEPSOBJS} \
-		$(sc2_OBJS) $(LDFLAGS)
+		$(sc2_OBJS) $(LDFLAGS) $(sc2_LDADD)
 

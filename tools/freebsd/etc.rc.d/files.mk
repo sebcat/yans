f2ee07e49ab4ddfa2450effa91334f1f5f57b311
Sebastian Cato <seb.cato@gmail.com>
2019-04-23 20:59:45 +0200

    tools/freebsd: Move tcpctl from skel to etc.rc.d

--- tools/freebsd/etc.rc.d/files.mk
+++ tools/freebsd/etc.rc.d/files.mk
@@ -5,7 +5,8 @@ GENERATED_RCFILESIN_FreeBSD = \
     tools/freebsd/etc.rc.d/clid.in \
     tools/freebsd/etc.rc.d/knegd.in \
     tools/freebsd/etc.rc.d/yansapi.in \
-    tools/freebsd/etc.rc.d/sysinfoapi.in
+    tools/freebsd/etc.rc.d/sysinfoapi.in \
+    tools/freebsd/etc.rc.d/tcpctl.in
 
 GENERATED_RCFILES_FreeBSD = ${GENERATED_RCFILESIN_FreeBSD:.in=}
 

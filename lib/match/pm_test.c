9d0a2d87925e5e3e96d830d55c92ce9c85f37880
Sebastian Cato <seb.cato@gmail.com>
2019-05-04 12:41:16 +0200

    pm_test: Add test_load_csv

--- lib/match/pm_test.c
+++ lib/match/pm_test.c
@@ -1,5 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
 #include <lib/match/pm.h>
 #include <lib/util/test.h>
+
+#define CSV_TEST_FILE "./data/pm/1.pm"
+#define CSV_MIN_ROWS 18 /* subject to change, update as needed */
 
 static int test_init_cleanup() {
   int ret;
@@ -15,6 +21,44 @@ static int test_init_cleanup() {
   return TEST_OK;
 }
 
+static int test_load_csv() {
+  int ret;
+  int status = TEST_FAIL;
+  struct pm_ctx pm;
+  size_t npatterns;
+  FILE *fp;
+
+  ret = pm_init(&pm);
+  if (ret != 0) {
+    TEST_LOGF("pm_init failure (%d)", ret);
+    return TEST_FAIL;
+  }
+
+  fp = fopen(CSV_TEST_FILE, "rb");
+  if (!fp) {
+    TEST_LOGF(CSV_TEST_FILE ": %s", strerror(errno));
+    goto done;
+  }
+
+  ret = pm_load_csv(&pm, fp, &npatterns);
+  if (ret != 0) {
+    TEST_LOG("failed to load patterns from CSV");
+    goto done;
+  }
+
+  if (npatterns < CSV_MIN_ROWS) {
+    TEST_LOGF("number of rows, min:%zu actual:%zu\n", (size_t)CSV_MIN_ROWS,
+        npatterns);
+    goto done;
+  }
+
+  status = TEST_OK;
+done:
+  pm_cleanup(&pm);
+  return status;
+}
+
 TEST_ENTRY(
   {"init_cleanup", test_init_cleanup},
+  {"load_csv", test_load_csv},
 );

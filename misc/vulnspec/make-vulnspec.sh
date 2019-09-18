55831599a39c889dc7dc73241dd59a0549f4f918
Sebastian Cato <seb.cato@gmail.com>
2019-09-18 07:37:37 +0200

    make-vulnspec.sh: Fix syntax errors

--- misc/vulnspec/make-vulnspec.sh
+++ misc/vulnspec/make-vulnspec.sh
@@ -2,7 +2,7 @@
 
 OUTFILE=components.vsi
 
-rm -f $(OUTFILE)
+rm -f "$OUTFILE"
 for YEAR in $(seq 2002 $(date '+%Y')); do
-  ./load.py "nvdcve-1.0-$YEAR.json.gz" >> $(OUTFILE)
+  ./load.py "nvdcve-1.0-$YEAR.json.gz" >> "$OUTFILE"
 done

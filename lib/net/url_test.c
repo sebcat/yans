0921e47351404b2d9c6ae89ff0e2020ac0d8adaf
Sebastian Cato <seb.cato@gmail.com>
2019-11-18 19:33:26 +0100

    url_test: Remove dead assignment

--- lib/net/url_test.c
+++ lib/net/url_test.c
@@ -123,7 +123,7 @@ static void url_parts_flagstr(int flags, char *buf, size_t len) {
   }
 
   if (flags & URLPART_HAS_FRAGMENT) {
-    next = snprintf(buf, len, "|URLPART_HAS_FRAGMENT");
+    snprintf(buf, len, "|URLPART_HAS_FRAGMENT");
   }
 }
 
